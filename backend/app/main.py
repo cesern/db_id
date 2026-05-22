77c273b042c3c0a5b71ecd2b19ceaf65bc6f80b6
Cesar Ernesto Salazar Buelna <cesar_0821@hotmail.com>
2026-05-21 19:18:39 -0700

    Temporary open CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.get_cors_origins_list,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
